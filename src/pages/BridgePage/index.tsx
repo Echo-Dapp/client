92717d41ad4a8f42c1505a5ee709af43de23f3ae
Riya Jain <riya1jain567@gmail.com>
2024-08-01 22:10:04 +0530

    added input data

--- src/pages/BridgePage/index.tsx
+++ src/pages/BridgePage/index.tsx
@@ -1,23 +1,33 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import Icon from "../../common/Icon";
+import { twMerge } from "tailwind-merge";
 
 export default function BridgePage() {
   const chains = [
     {
-      name: "Ethereum",
-      logo: "/public/icons/eth.png",
+      name: "Base",
+      logo: "/public/icons/base.png",
+      token: "ETH",
     },
     {
       name: "EduChain",
       logo: "/public/icons/eduChain.png",
+      token: "WETH",
+    },
+    {
+      name: "Ethereum",
+      logo: "/public/icons/eth.png",
+      token: "ETH",
+    },
+    {
+      name: "Fraxtal",
+      logo: "/public/icons/fraxtal.webp",
+      token: "frxETH",
     },
     {
       name: "Optimism",
       logo: "/public/icons/optimism.png",
-    },
-    {
-      name: "fraxtal",
-      logo: "/public/icons/fraxtal.webp",
+      token: "oETH",
     },
   ];
 
@@ -53,6 +63,11 @@ export default function BridgePage() {
     return chain ? chain.logo : "";
   };
 
+  const getSelectedChainToken = (chainName: string) => {
+    const chain = chains.find((c) => c.name === chainName);
+    return chain ? chain.token : "Select chain first";
+  };
+
   const isFormValid = () => {
     if (isTransferToOther) {
       return transferFrom && transferTo && destinationAddress;
@@ -72,9 +87,14 @@ export default function BridgePage() {
         <div className="flex flex-col gap-y-6">
           <div className="flex flex-col gap-y-2">
             <h2>Transfer From</h2>
-            <div className="relative">
+            <div className="flex items-center gap-x-4 relative">
+              <input
+                className={twMerge("w-1/2 rounded-lg bg-transparent p-3 border border-front/20")}
+                placeholder={getSelectedChainToken(transferFrom)}
+                disabled={!transferFrom}
+              />
               <div
-                className="w-full bg-transparent p-3 rounded-lg border border-front/20 cursor-pointer flex items-center"
+                className="w-1/2 bg-transparent p-3 rounded-lg border border-front/20 cursor-pointer flex items-center"
                 onClick={() => setIsFromDropdownOpen(!isFromDropdownOpen)}
               >
                 {transferFrom ? (
@@ -94,7 +114,7 @@ export default function BridgePage() {
                 )}
               </div>
               {isFromDropdownOpen && (
-                <div className="absolute w-full bg-background rounded-lg shadow-lg z-10 mt-1">
+                <div className="absolute left-1/2 transform -translate-x-1/2 w-1/2 bg-background rounded-lg shadow-lg z-10 mt-1 border border-front/20">
                   {chains.map((chain, i) => (
                     <div
                       key={i}
@@ -122,9 +142,14 @@ export default function BridgePage() {
           </div>
           <div className="flex flex-col gap-y-2">
             <h2>Transfer To</h2>
-            <div className="relative">
+            <div className="flex items-center gap-x-4 relative">
+              <input
+                className={twMerge("w-1/2 rounded-lg bg-transparent p-3 border border-front/20")}
+                placeholder={getSelectedChainToken(transferTo)}
+                disabled={!transferTo}
+              />
               <div
-                className="w-full bg-transparent p-3 rounded-lg border border-front/20 cursor-pointer flex items-center"
+                className="w-1/2 bg-transparent p-3 rounded-lg border border-front/20 cursor-pointer flex items-center"
                 onClick={() => setIsToDropdownOpen(!isToDropdownOpen)}
               >
                 {transferTo ? (
@@ -144,7 +169,7 @@ export default function BridgePage() {
                 )}
               </div>
               {isToDropdownOpen && (
-                <div className="absolute w-full bg-background rounded-lg shadow-lg z-10 mt-1">
+                <div className="absolute left-1/2 transform -translate-x-1/2 w-1/2 bg-background rounded-lg shadow-lg z-10 mt-1 border border-front/20">
                   {chains.map((chain, i) => (
                     <div
                       key={i}
@@ -181,7 +206,7 @@ export default function BridgePage() {
           </div>
           {isTransferToOther && (
             <input
-              className="bg-transparent rounded-lg p-3"
+              className="bg-transparent rounded-lg p-3 border border-front/20"
               placeholder="Destination address"
               value={destinationAddress}
               onChange={(e) => setDestinationAddress(e.target.value)}
@@ -198,12 +223,12 @@ export default function BridgePage() {
         </button>
       </div>
       <div>
-        <div className="absolute bottom-0 right-0 w-32 aspect-square bg-secondary/20" />
+        <div className="absolute bottom-0 right-0 w-32 aspect-square bg-secondary/10" />
         <div className="absolute bottom-0 right-64 w-32 aspect-square bg-primary/10" />
         <div className="absolute bottom-48 right-64 w-32 aspect-square bg-secondary/5" />
         <div className="absolute bottom-16 right-96 w-32  aspect-square bg-primary/5" />
         <div className="absolute bottom-16 right-32 w-32 aspect-square bg-primary/10" />
-        <div className="absolute bottom-96 right-0 w-32  aspect-square bg-secondary/10" />
+        <div className="absolute bottom-96 right-0 w-32  aspect-square bg-secondary/5" />
         <div className="absolute bottom-48 right-0 w-32  aspect-square bg-primary/10" />
         <div className="absolute bottom-96 right-64 w-32  aspect-square bg-primary/5" />
         <div className="absolute bottom-64 right-32 w-32  aspect-square bg-primary/10" />
